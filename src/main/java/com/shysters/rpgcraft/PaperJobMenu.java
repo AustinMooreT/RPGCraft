acf995f493bf9c0183ccd25c7d5cb9fce3a0a43b
Austin Moore <mooreat2@miamioh.edu>
2021-04-11 11:37:36 -0400

    tinkers with texture loading and this blank card texture I made.

--- src/main/java/com/shysters/rpgcraft/PaperJobMenu.java
+++ src/main/java/com/shysters/rpgcraft/PaperJobMenu.java
@@ -9,9 +9,7 @@ import net.minecraft.util.text.StringTextComponent;
 
 public class PaperJobMenu extends Screen {
 
-    private final ResourceLocation FUBUKI_TEST = new ResourceLocation("rpgcraft","textures/fubuki_test.png");
-    private final int FUBUKI_WIDTH = 256;
-    private final int FUBUKI_HEIGHT = 256;
+    private final ResourceLocation BLANK_CARD = new ResourceLocation("rpgcraft","textures/blank_card_128x128.png");
     
     public PaperJobMenu() {
 	super(new StringTextComponent("Job Menu"));
@@ -23,26 +21,24 @@ public class PaperJobMenu extends Screen {
       I just copied this from the death screen code.
     */
     public void render(MatrixStack ms, int i1, int i2, float f) {
-
-	// Renders Fubuki?
-	renderFubuki(ms);
-
 	//Renders jobmenu text and dead red gradient
-	this.fillGradient(ms, 0, 0, this.width, this.height, 1615855616, -1602211792);
-	RenderSystem.pushMatrix();
-	RenderSystem.scalef(2.0F, 2.0F, 2.0F);
-	drawCenteredString(ms, this.font, this.title, this.width / 2 / 2, 30, 16777215);
-	RenderSystem.popMatrix();
-	
+	this.fillGradient(ms, 0, 0, this.width, this.height, 2130706433, 2130706433);
+	renderFubuki(ms, 150, 100);
+	drawCenteredString(ms, this.font, this.title, this.width / 2, this.height / 2, 16777215);
 	super.render(ms, i1, i2, f);
     }
 
-    private void renderFubuki(MatrixStack ms) {
+    private void renderFubuki(MatrixStack ms, int x, int y) {
 	RenderSystem.color4f(1.0F, 1.0F, 1.0F, 1.0F);
-	this.minecraft.getTextureManager().bind(this.FUBUKI_TEST);
-	int i = (this.width - this.FUBUKI_WIDTH) / 2;
-	int j = (this.height - this.FUBUKI_HEIGHT) / 2;
-	this.blit(ms, i, j, 0, 0, this.FUBUKI_WIDTH, this.FUBUKI_HEIGHT);
+	this.minecraft.getTextureManager().bind(this.BLANK_CARD);
+	RenderSystem.pushMatrix();
+	//float xscale = ((float)x)/256.0F;
+	//float yscale = ((float)y)/256.0F;
+	//RenderSystem.scalef(xscale, yscale, 0);
+	int i = (this.width - 128) / 2;
+	int j = (this.height - 128) / 2;
+	this.blit(ms, i, j, 0, 0, 128, 128);
+	RenderSystem.popMatrix();
     }
 
     //TODO figure out what d1, d2, and i are.
